61fcaa7234b120030fc80a1e0a00aa3df7bde307
Leila Ouahrani <66189111+leilaouahrani@users.noreply.github.com>
2020-06-26 20:40:39 +0200

    Update qtype_englishanswer1.php

--- English Short Answer Plugin/englishanswer1/lang/ar/qtype_englishanswer1.php
+++ English Short Answer Plugin/englishanswer1/lang/ar/qtype_englishanswer1.php
@@ -18,14 +18,14 @@
  * Defines the editing form for the arabicanswer question type.
  *
  * @package    qtype
- * @subpackage arabicanswer1
+ * @subpackage englishcanswer1
  * @copyright  2019 Snoussi El Hareth & Madani Abderraouf For C00L07UN100120180002 Project
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 $string['pluginname'] = 'اجابة قصيرة دلالية';
 $string['pluginname_help'] = 'يجب عليك إدخال إجابة قصيرة من خلال استهداف المفهوم المدروس مباشرة';
-$string['pluginname_link'] = 'question/type/arabicanswer1';
+$string['pluginname_link'] = 'question/type/englishanswer1';
 $string['pluginnameadding'] = 'إضافة سؤال';
 $string['pluginnameediting'] = 'تعديل سؤال';
 $string['pluginnamesummary'] = 'سؤال يقوم تلقائيًا بتعيين استجابات الطلاب بناءً على التشابه الدلالي مع استجابة نموذج المعلم.';
